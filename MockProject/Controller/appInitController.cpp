e6a91554ccee7f7a460a5bcf301b17ae5345872f
PhamQuangHa2301 <pqh230102fw@gmail.com>
2025-08-26 14:51:12 +0700

    Delete MockProject directory

--- MockProject/Controller/appInitController.cpp
+++
@@ -1,8 +0,0 @@
-#include "appInitController.h"
-
-void appInitController::appInitFunction(appData& data, std::string& filename){
-    sharedControllerFunction::loadMediaFileLib(data, filename);
-    sharedControllerFunction::loadMediaFileToPlaylist(filename);
-    sharedControllerFunction::loadPlaylist(data, filename);
-    data.setControlMode(MAIN_SCREEN);
-}
